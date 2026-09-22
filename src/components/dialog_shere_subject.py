c50716582b564003123461b90659e5ed193596bc
Sumersing Ahire <sumersing.ahire.321@gmail.com>
2026-09-22 14:48:36 +0530

    Update url

--- src/components/dialog_shere_subject.py
+++ src/components/dialog_shere_subject.py
@@ -5,7 +5,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name , subject_code):
-    app_domain = "https://1026snapclass-ai.streamlit.app"
+    app_domain = "https://1026snapclass.streamlit.app"
     join_url = f"{app_domain}/?join_code={subject_code}"
 
     st.header("Scan to join")
